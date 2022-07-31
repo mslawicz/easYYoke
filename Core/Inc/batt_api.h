a4bcb26a9bc4c64746a92d7074e704b8837375e6
Marcin Sławicz <marcin.slawicz@gmail.com>
2022-07-31 10:55:40 +0200

    battery files added

---
+++ Core/Inc/batt_api.h
@@ -0,0 +1,13 @@
+/*
+ * batt_api.h
+ *
+ *  Created on: 31 lip 2022
+ *      Author: marci
+ */
+
+#ifndef INC_BATT_API_H_
+#define INC_BATT_API_H_
+
+void setBatteryLevel(void);
+
+#endif /* INC_BATT_API_H_ */
---
+++ Core/Inc/battery.h
@@ -0,0 +1,15 @@
+/*
+ * battery.h
+ *
+ *  Created on: 31 lip 2022
+ *      Author: marci
+ */
+
+#ifndef INC_BATTERY_H_
+#define INC_BATTERY_H_
+
+
+
+
+
+#endif /* INC_BATTERY_H_ */
